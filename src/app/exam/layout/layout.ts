f9741dc3139d87228ce178f5a4dacbfc2e3190e8
jobson-okosun <jobsonwesly1@gmail.com>
2026-07-05 12:13:54 +0100

    Test drawing and writing

--- src/app/exam/layout/layout.ts
+++ src/app/exam/layout/layout.ts
@@ -224,10 +224,10 @@ export default class Layout implements OnDestroy {
     this.isMobile.set(window.matchMedia('(max-width: 768px)').matches)
     this.updateDrawingAndWritingLayoutConfigInStore()
 
-    // if (!this.store().loginData) {
-    //   this._authService.setPreLoginData(preloginData as any);
-    //   this._postLoginService.formatLoginDataToStore(loginData as any)
-    // }
+    if (!this.store().loginData) {
+      this._authService.setPreLoginData(preloginData as any);
+      this._postLoginService.formatLoginDataToStore(loginData as any)
+    }
 
     if (!this.store().platformIsTauri) {
       fullscreen(this._eventService)
