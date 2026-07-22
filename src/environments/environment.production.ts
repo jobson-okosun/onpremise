d079f46225953399f19df1e79b98f39227770bc5
jobson-okosun <jobsonwesly1@gmail.com>
2026-07-22 15:33:36 +0100

    Build: Prep for exam

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,9 +1,10 @@
 export const environment = {
     production: true,
-    // domain: 'https://onpremise.ngrok.app',
-    // developmentIP: "https://onpremise.ngrok.app/examalpha/onpremise",
-    developmentIP: location.protocol.concat('//', location.hostname, location.port ? `:${location.port}` : ':80', '/examalpha/onpremise'),
-    domain: location.protocol.concat('//', location.hostname, location.port ? `:${location.port}` : ':80'),
+    domain: 'https://onpremise.ngrok.app',
+    developmentIP: "https://onpremise.ngrok.app/examalpha/onpremise",
+    // developmentIP: location.protocol.concat('//', location.hostname, location.port ? `:${location.port}` : ':80', '/examalpha/onpremise'),
+    // domain: location.protocol.concat('//', location.hostname, location.port ? `:${location.port}` : ':80'),
+    
     NETWORK_CHECK: {
         DOWNLOAD: "https://pub-086a38f5ef8c44ccaf276c5b1a970832.r2.dev/2mb.bin",
         UPLOAD: "https://ml-server-api.ngrok.app/ml_server/upload-speed",
